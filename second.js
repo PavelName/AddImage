90cb741ec9e1ad4f1d23bdfd47ed4a89ed446271
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2025-03-28 16:44:47 +0800

    second exersises

--- second.js
+++ second.js
@@ -1,1 +1,16 @@
-console.log('hi!');
+let num = 266219;
+let prod = 1;
+
+for ( let i = 0; i < num.toString().length; i++) {
+  prod *= parseInt(num.toString()[i]);
+}
+
+console.log(prod);
+
+console.log(prod ** 3);
+console.log ();
+
+let prime = '123';
+let result = parseInt(prime)
+console.log(typeof prime);
+console.log(typeof result);
