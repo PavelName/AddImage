8e17023819f49375c83269d5a76874e1179369d9
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2025-03-30 20:23:42 +0800

    a diffikalt task has been complated

--- second.js
+++ second.js
@@ -3,7 +3,7 @@ let namePerson = 'Alexander';
 let result = (namePerson === 'Artem')? console.log('директор'):
             (namePerson === 'Alexander') ? console.log('преподаватель'): console.log('студент');
 
-let leng = 'en';
+let leng = 'ru';
 let weeks = '';
 if(leng === 'ru') {
   weeks = 'пон, вт,ср, чт, пт, сб, вс';
@@ -13,4 +13,27 @@ if(leng === 'ru') {
   weeks = 'not languges supported!';
 }
 console.log(weeks);
+
+let len = 'en';
              
+switch (len) {
+  case 'ru':
+    weeks = 'пон, вт,ср, чт, пт, сб, вс';
+    break;
+  case 'en':
+    weeks = 'mon, tu, we, thu, fr, sat, sun';
+    break;
+  default:
+    weeks = 'Not days!';
+
+}
+console.log(weeks);
+
+let languges = "en";
+const daysOfWeeks = [
+  ['пон, вт,ср, чт, пт, сб, вс', 'hello'],
+  ['mon, tu, we, thu, fr, sat, sun', 'goodbye']
+];
+const langIndex = languges === 'ru' ? 0 : 1;
+const results = daysOfWeeks[langIndex].join(', ');
+console.log(results);
